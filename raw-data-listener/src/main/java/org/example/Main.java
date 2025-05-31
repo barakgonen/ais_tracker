fa350293f9e32144cf6fb86e56957f26a3736b5d
bg <barakgonen9@gmail.com>
2025-05-31 09:50:24 +0300

    Added avro messages for communication between raw-data-retriever to raw-data-listener

--- raw-data-listener/src/main/java/org/example/Main.java
+++ raw-data-listener/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import com.ais.avro.schemas.ConnectionStatus;
+import com.ais.avro.schemas.Heartbeat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
     public static void main(String[] args) {
+        Heartbeat heartbeat = Heartbeat.newBuilder()
+                .setStatus(ConnectionStatus.CONNECTED)
+                .build();
         SpringApplication.run(Main.class, args);
     }
 }
