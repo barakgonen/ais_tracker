98655ba01c51198e82891e233a5e87dc622f27f5
bg <barakgonen9@gmail.com>
2025-05-31 16:21:20 +0300

    There is a producer

--- raw-data-fetcher/src/main/java/org/example/KafkaProducerConfig.java
+++ raw-data-fetcher/src/main/java/org/example/KafkaProducerConfig.java
@@ -1,2 +1,15 @@
-package org.example;public class KafkaProducerConfig {
+package org.example;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+public class KafkaProducerConfig {
+
+    @Value("${topic}")
+    private String rawDataTopic;
 }
--- raw-data-fetcher/src/main/java/org/example/RawDataProducer.java
+++ raw-data-fetcher/src/main/java/org/example/RawDataProducer.java
@@ -1,2 +1,36 @@
-package org.example;public class RawDataProducer {
+package org.example;
+
+import com.ais.avro.schemas.ConnectionStatus;
+import com.ais.avro.schemas.Heartbeat;
+import com.ais.avro.schemas.MessageWrapper;
+import lombok.AllArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class RawDataProducer {
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final KafkaProducerConfig configProperties;
+
+
+    @Scheduled(fixedRate = 1000)
+    public void sendMessage() {
+        var timeStamp = Instant.now().toEpochMilli();
+        Heartbeat bg = Heartbeat.newBuilder()
+                .setStatus(ConnectionStatus.CONNECTED)
+                .setTimestamp(timeStamp)
+                .build();
+        var message = MessageWrapper.newBuilder()
+                .setTimestamp(timeStamp)
+                .setBg(bg)
+                .build();
+
+        kafkaTemplate.send("vf", UUID.randomUUID().toString(), message);
+        System.out.println("Produced: " + message);
+    }
 }
